16c6548c8346367d3b7090bccf0b18f6ba415c2f
ferd <ferd@ferd-laptop.(none)>
2010-10-26 21:43:37 +0800

    modified d1 template

--- d7/sites/all/themes/d1/page.tpl.php
+++ d7/sites/all/themes/d1/page.tpl.php
@@ -1,21 +1,44 @@
 <?php
-print render($page['header']);
+//print(render($page["header"]));
 ?>
 
   <div id="wrapper">
     <div id="container" class="clearfix">
     
     	<div id="header">
+    		<h1>Header</h1>
+			<?php print(render($page["header"])); ?>
     	</div> <!-- /#header -->
-    	
-    	<div id="content">
-    		<div id="content-messages">
-    			<?php print $messages; ?>
-    		</div>	<!-- /#content-messages -->
-    		<?php print render($page['content']); ?>
-    	</div> <!-- /#content -->
+
+		<div id="center">
+		 	
+		 	<div id="sidebarleft">
+		 		<h1>Left</h1>
+		 		<?php 
+		 		if($page["left"]){
+		 			print(render($page["left"]));
+		 		}
+		 		?>
+		 	</div> <!-- /#sidebarleft -->
+		 	
+	    	<div id="content">
+	    		<div id="content-messages">
+	    			<?php print $messages; ?>
+	    		</div>	<!-- /#content-messages -->
+	    		<?php print(render($page["content"])); ?>
+	    	</div> <!-- /#content -->
+	    	
+	    	<div id="sidebarright">
+	    		<h1>Right</h1>
+	    		<?php print(render($page["right"])); ?>
+	    	</div> <!-- /#sidebarright -->
+	    	
+	    	<div class="clearfix"></div>
+	    	
+	    </div> <!-- /#center -->
     	
     	<div id="footer">
+    		<h1>Footer</h1>
     	</div>	<!-- /#footer -->
 
     </div> <!-- /#container -->
